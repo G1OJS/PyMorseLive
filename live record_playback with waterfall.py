84fba5b46e2d14487eb726329f742d1d9f8d04c8
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-01-24 23:20:43 +0000

    Update live record_playback with waterfall.py

--- live record_playback with waterfall.py
+++ live record_playback with waterfall.py
@@ -6,12 +6,12 @@ import threading
 
 global audio_buff, out_stream
 
-waterfall_duration = 2
-waterfall_dt = 0.0005
+waterfall_duration = 1
+waterfall_dt = 0.001
 waterfall_update_dt = 0.1
-sample_rate = 6000
-max_freq = 2000
-waterfall_df = 20
+sample_rate = 8000
+max_freq = 1000
+waterfall_df = 50
 fft_len = int(sample_rate / waterfall_df)
 nFreqs = int(max_freq / waterfall_df)
 
@@ -46,7 +46,7 @@ def start_live(input_device_idx, output_device_idx):
                           output=True,
                           output_device_index = output_device_idx)
     threading.Thread(target = threaded_output).start()
-    threading.Thread(target = threaded_get_sigs).start()
+    threading.Thread(target = threaded_get_key).start()
 
 def threaded_output():
     global audio_buff
@@ -63,12 +63,24 @@ def _callback(in_data, frame_count, time_info, status_flags):
     time.sleep(0)
     return (None, pyaudio.paContinue)
 
-
-global waterfall
+global waterfall, key
 waterfall = np.zeros((int(waterfall_duration / waterfall_dt), nFreqs))
-def threaded_get_sigs():
-    global waterfall
+
+def threaded_get_key():
+    global waterfall, key
     speclev = 1
+    
+    def hysteresis(signal, lo=0.3, hi=0.6):
+        out = np.zeros_like(signal, dtype=bool)
+        state = False
+        for i, v in enumerate(signal):
+            if not state and v > hi:
+                state = True
+            elif state and v < lo:
+                state = False
+            out[i] = state
+        return out
+
     while(True):
         time.sleep(waterfall_dt)
         z = np.fft.rfft(audio_buff)[:nFreqs]
@@ -77,17 +89,20 @@ def threaded_get_sigs():
         p /= speclev
         waterfall[:-1,:] = waterfall[1:,:] 
         waterfall[-1,:] = np.clip(10*p, 0, 1)
+        key = hysteresis(waterfall[:,int(waterfall.shape[1]/2)])
 
 def time_plot():
     import matplotlib.pyplot as plt
-    fig, axs = plt.subplots(figsize = (8,3))
-    waterfall_plot = axs.imshow(waterfall, extent = (0, waterfall.shape[1]*50, 0, waterfall.shape[0]))
-
-    #axs[0].set_xticks(np.array([95,105])-0.5, labels=["",""])
-    #axs[0].set_ylim(-10,0)
+    fig, axs = plt.subplots(2,1, figsize = (8,3))
+    waterfall_plot = axs[0].imshow(waterfall, extent = (0, waterfall.shape[1]*50, 0, waterfall.shape[0]))
+    key_plot, = axs[1].plot(key)
+    axs[1].set_ylim(0,2)
     while(True):
         waterfall_plot.set_data(waterfall)
+        key_plot.set_ydata(key)
         plt.pause(waterfall_update_dt)
+
+        time.sleep(0.001)
 
     
 input_device_idx =  find_device(['Mic', 'CODEC'])
